8f26d28b9a7427eac5dcc3c29ebcea207e0be139
Abel <abel.carriel@icloud.com>
2025-02-02 09:41:06 -0300

    Removido codigo desnecessario

--- src/main/java/com/payconferhub/repository/PlanRepository.java
+++
@@ -1,7 +0,0 @@
-package com.payconferhub.repository;
-
-import com.payconferhub.model.Plan;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlanRepository extends JpaRepository<Plan, Long> {
-}
